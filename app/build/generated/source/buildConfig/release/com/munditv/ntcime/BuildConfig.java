1fa2f7f8e39e9654bee45a65111fbc5a11bcc13e
林政毅 <chevylin0802@gmail.com>
2019-11-09 16:55:59 +0800

    Update gradle from 3.5.1 to 3.5.2

--- app/build/generated/source/buildConfig/release/com/munditv/ntcime/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/com/munditv/ntcime/BuildConfig.java
@@ -9,5 +9,5 @@ public final class BuildConfig {
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 113;
-  public static final String VERSION_NAME = "0.8.0.10.8v0.8.2";
+  public static final String VERSION_NAME = "0.9.20.9v0.9.2";
 }
